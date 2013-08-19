92e811c0927fb83efac08f1701f74d539a909c5d
Cy Brown <cy@cybrown.fr>
2013-08-19 20:30:45 +0200

    Debuggage du model Game

--- app/models/Game.java
+++ app/models/Game.java
@@ -1,28 +1,48 @@
 package models;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import exceptions.AlreadyInGameException;
+import org.hibernate.annotations.Fetch;
+
 import play.db.jpa.JPA;
+import exceptions.AlreadyInGameException;
+import exceptions.TooManyPlayersException;
 
 @Entity
 public class Game {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
 	@Column
 	private String name;
 	
-	@OneToMany(mappedBy="game")
+	@OneToMany(mappedBy="game", fetch=FetchType.LAZY)
 	private List<User> players;
 
-	public void addPlayer(User user) throws AlreadyInGameException {
+	public Game() {
+		this.players = new LinkedList<User>();
+	}
+	
+	public void addFirstPlayer(User user) throws AlreadyInGameException {
+		user.setGame(this);
+		this.players.add(user);
+	}
+	
+	public void addPlayer(User user) throws AlreadyInGameException, TooManyPlayersException {
+		if (this.players.size() >= 5) {
+			throw new TooManyPlayersException();
+		}
 		user.setGame(this);
 		this.players.add(user);
 	}
@@ -31,6 +51,9 @@ public class Game {
 		if (this.players.contains(user)) {
 			user.removeGame();
 			this.players.remove(user);
+			if (this.players.size() == 0) {
+				JPA.em().remove(this);
+			}
 		}
 	}
 	
