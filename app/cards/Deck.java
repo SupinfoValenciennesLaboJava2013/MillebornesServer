d919415421f9a9c9cb48369edb00479985609e8d
Cy Brown <cy@cybrown.fr>
2013-09-17 21:06:41 +0200

    Ajout d'une méthode shuffle pour le deck

--- app/cards/Deck.java
+++ app/cards/Deck.java
@@ -1,12 +1,23 @@
 package cards;
 
-public class Deck {
+import java.util.LinkedList;
+import java.util.List;
 
-	private Card _deck[] = new Card[106];
+public class Deck {
+	
+	private List<Card> deck = new LinkedList<Card>();
 	
 	public Deck()
 	{
 		
+	}
+	
+	public void shuffle() {
+		List<Card> deck2 = new LinkedList<Card>();
+		while (!this.deck.isEmpty()) {
+			deck2.add(this.deck.get((int)Math.floor(Math.random() * this.deck.size())));
+		}
+		this.deck = deck2;
 	}
 	
 }
