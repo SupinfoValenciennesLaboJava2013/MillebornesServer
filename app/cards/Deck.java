701216ba78ab289ced5b2ef8eb6a1f5377179431
L3m4c <samuel.leroy@supinfo.com>
2013-09-18 09:37:06 +0200

    Update Deck.java
    Création du Deck : 106 Cartes :18 cartes Attaque, 38 cartes Parade, 4 cartes Botte, 46 cartes Distance

--- app/cards/Deck.java
+++ app/cards/Deck.java
@@ -9,31 +9,49 @@ public class Deck {
 	
 	public Deck()
 	{
+		//ajout des Safeties
 		deck.add(new DrivingAce());
 		deck.add(new ExtraTank());
 		deck.add(new PunctureProof());
 		deck.add(new RightOfWay());
 		
+		//ajout d'Attaque
 		for(int i=0;i<3;i++)
 		{
 			deck.add(new Accident());
 			deck.add(new FlatTire());
 			deck.add(new OutOfGas());
 		}
-		
+		//ajout d'Attaque et de Distance
 		for(int i=0;i<4;i++)
 		{
 			deck.add(new SpeedLimit());
+			deck.add(Distance.get200km());
 		}
+		//ajout D'attaque
 		for(int i=0;i<5;i++)
-		{
 			deck.add(new Stop());
-		}
+		//ajout des reparations
 		for(int i=0;i<6;i++)
 		{
-			deck.add();
-				deck.add();
+			deck.add(new Repairs());
+			deck.add(new EndOfLimit());
+			deck.add(new Gasoline());
+			deck.add(new SpareTire());
 		}
+		//ajout de distances
+		for(int i=0;i<10;i++)
+		{
+			deck.add(Distance.get25km());
+			deck.add(Distance.get50km());
+			deck.add(Distance.get75km());
+		}
+		//ajout de distances
+		for(int i=0;i<12;i++)
+			deck.add(Distance.get100km());
+		//ajout de feu vert	
+		for(int i=0;i<14;i++)
+			deck.add(new GoRoll());
 	}
 	
 	public void shuffle() {
