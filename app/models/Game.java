1dae10f26181b11cae676982cfa43784835d6d47
Cy Brown <cy@cybrown.fr>
2013-09-18 22:51:20 +0200

    Oublie d'un import -_-'

--- app/models/Game.java
+++ app/models/Game.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
 import play.db.jpa.JPA;
 import exceptions.AlreadyInGameException;
