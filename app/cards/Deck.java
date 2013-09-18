debd4a8559725c81810871636bb399c8aca17bf8
L3m4c <samuel.leroy@supinfo.com>
2013-09-18 09:17:09 +0200

    Update Deck.java

--- app/cards/Deck.java
+++ app/cards/Deck.java
@@ -9,7 +9,31 @@ public class Deck {
 	
 	public Deck()
 	{
+		deck.add(new DrivingAce());
+		deck.add(new ExtraTank());
+		deck.add(new PunctureProof());
+		deck.add(new RightOfWay());
 		
+		for(int i=0;i<3;i++)
+		{
+			deck.add(new Accident());
+			deck.add(new FlatTire());
+			deck.add(new OutOfGas());
+		}
+		
+		for(int i=0;i<4;i++)
+		{
+			deck.add(new SpeedLimit());
+		}
+		for(int i=0;i<5;i++)
+		{
+			deck.add(new Stop());
+		}
+		for(int i=0;i<6;i++)
+		{
+			deck.add();
+				deck.add();
+		}
 	}
 	
 	public void shuffle() {
