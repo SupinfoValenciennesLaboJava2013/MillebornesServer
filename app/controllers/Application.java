c908b705da7811bb79fdf7fea9dff8417746db7b
Cy Brown <cy@cybrown.fr>
2013-09-22 23:06:00 +0200

    Ajout de /echo

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,9 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.Http.RawBuffer;
+import play.mvc.Http.Request;
+import play.mvc.Http.RequestBody;
 import play.mvc.Result;
 
 public class Application extends Controller {
@@ -9,4 +12,11 @@ public class Application extends Controller {
         return ok("Your new application is ready.");
     }
   
+    public static Result echo() {
+    	Request request = request();
+    	RequestBody body = request.body();
+    	RawBuffer raw = body.asRaw();
+    	return ok(raw.asBytes());
+    }
+    
 }
